82b5ca70c5ce1592299b118a27c04f42141e9c53
Carlos Caicedo <calicheccmusica@gmail.com>
2022-01-22 22:22:31 -0800

    second commit

--- flask_app/controllers/user_controller.py
+++ flask_app/controllers/user_controller.py
@@ -1,8 +1,8 @@
-from flask import render_template, redirect, request, session
-
 from flask_app import app
-
+from flask import render_template,redirect,request,session
 from flask_app.models.user import User
+
+
 
 @app.route('/')
 @app.route("/users")
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -1,3 +1,5 @@
+
+
 from flask_app.config.mysqlconnection import connectToMySQL
 
 class User:
